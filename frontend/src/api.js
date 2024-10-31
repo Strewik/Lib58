390186ba4096f5cf795075dedace73ac3edb733f
Kawooya Steven <kawooyastevenug@gmail.com>
2024-10-31 17:33:46 +0300

    fixes baseURL for remote

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,8 +4,8 @@ import { ACCESS_TOKEN } from "./constants";
 const isDevelopment  = import.meta.env.NODE === "development"
 const api = axios.create({
     // baseURL: "http://localhost:8000"
-    baseURL: isDevelopment ? import.meta.env.VITE_API_URL_LOCAL : import.meta.env.VITE_API_URL_DEPLOY
-
+    // baseURL: isDevelopment ? import.meta.env.VITE_API_URL_LOCAL : import.meta.env.VITE_API_URL_DEPLOY
+    baseURL: "https://lib58.onrender.com"
 })
 
 api.interceptors.request.use(

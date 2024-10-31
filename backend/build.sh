2e91ffdfc690bec13eb3de2f3e70c15a5973c932
Kawooya Steven <kawooyastevenug@gmail.com>
2024-10-31 03:16:09 +0300

    some sort of user

--- backend/build.sh
+++ backend/build.sh
@@ -6,8 +6,8 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-if [[ $CREATE_SUPERUSER ]]
-then
-    python manage.py createsuperuser --full_name --no-input
-fi
+# if [[ $CREATE_SUPERUSER ]]
+# then
+#     python manage.py createsuperuser --full_name --no-input
+# fi
 
